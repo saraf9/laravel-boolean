0026098a4a5d5dfc981d881865119354f9352110
saraf9 <fornonisara9@gmail.com>
2019-05-20 20:05:26 +0200

    Start header

---
+++ resources/views/boolean/layout/home-layout.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://fonts.googleapis.com/css?family=Rubik&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="/css/app.css">
+        <link rel="shortcut icon" href="https://www.boolean.careers/images/favicon/favicon.ico" />
+        <title>Il corso per diventare web developer e trovare un nuovo lavoro</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Rubik:300,400&display=swap" rel="stylesheet">
+    </head>
+    <body>
+      <div class="wrapper">
+        <!-- INIZIO HEADER -->
+        <header>
+          <div class="logo">
+            <img src="https://www.boolean.careers/images/common/logo.png" alt="">
+          </div>
+          <div class="navbar">
+            <ul class="nav-menu">
+              <li class="nav-item"><a href="#">Home</a></li>
+              <li class="nav-item"><a href="#">Corso</a></li>
+              <li class="nav-item"><a href="#">Metodo</a></li>
+              <li class="nav-item"><a href="#">Dopo il corso</a></li>
+              <li class="nav-item"><a href="#">Faq</a></li>
+              <li class="btn-signup"><a href="#">Iscriviti</a></li></li>
+            </ul>
+          </div>
+        </header>
+        <!-- FINE HEADER INIZIO CONTENT -->
+        <!-- FINE CONTENT INIZIO FOOTER -->
+        <!-- FINE FOOTER -->
+      </div>
+    </body>
+</html>
